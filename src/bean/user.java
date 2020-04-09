1ad45b7ed0e5b3209ba45a3e9641df6c27b21dcb
yudeshuai <>
2020-04-09 15:22:42 +0800

    修改的

--- src/bean/user.java
+++ src/bean/user.java
@@ -3,4 +3,28 @@ package bean;
 public class user {
     private int id;
     private  String name;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public String toString() {
+        return "user{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
